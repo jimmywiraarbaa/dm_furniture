b2c51d328af68eccdb0cb2e5da380df9ae8ee7d4
Jimmy Wira Arbaa <jimmywiraarbaa03@gmail.com>
2024-08-19 11:37:42 +0700

    Membuat Components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Browse from './components/Browse';
+import Arrived from './components/Arrived';
+import Clients from './components/Clients';
+import AsideMenu from './components/AsideMenu';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <p className="font-bold underline">Halo</p>
+    <>
+    <Header/>
+    <Hero/>
+    <Browse/>
+    <Arrived/>
+    <Clients/>
+    <AsideMenu/>
+    <Footer/>
+    </>
   );
 }
 
